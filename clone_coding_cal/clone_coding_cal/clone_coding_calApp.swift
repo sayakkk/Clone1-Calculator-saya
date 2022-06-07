ee8928358c0ca36a2ff7b953c4bbb6cbdac3c5f9
sayalion <dltkdi173@gmail.com>
2022-06-08 02:03:19 +0900

    Add: "계산기 UI build"
    1. 한자릿수만 작동
    2. 사칙연산 안됨

---
+++ clone_coding_cal/clone_coding_cal/clone_coding_calApp.swift
@@ -0,0 +1,17 @@
+//
+//  clone_coding_calApp.swift
+//  clone_coding_cal
+//
+//  Created by 이사야 on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct clone_coding_calApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
